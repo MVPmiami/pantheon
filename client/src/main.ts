42b1d6c2c6fa283819826145eba8cc785f00bf43
Aliaksei Siankevich <asenkevich1994@gmail.com>
2023-04-26 11:16:25 +0300

    master: change framework

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,8 +1,5 @@
-import './app.css'
-import App from './App.svelte'
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
 
-const app = new App({
-  target: document.getElementById('app'),
-})
-
-export default app
+createApp(App).mount('#app')
