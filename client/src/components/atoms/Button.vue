00501ac0fc2297ab946be69ca7a944bde46e0750
Aliaksei Siankevich <asenkevich1994@gmail.com>
2023-04-29 11:40:07 +0300

    master: added adaptive, logo

--- client/src/components/atoms/Button.vue
+++ client/src/components/atoms/Button.vue
@@ -4,38 +4,69 @@ const props = defineProps({
 })
 </script>
 <template>
-    <button :class='[$style.btn, $style.animation,
-    {
-       [$style.red]: props.color === "red" 
-    },
-    {
-       [$style.grey]: props.color === "grey" 
-    },
-    { 
-      [$style.orange]: props.color === "orange" 
-    } ]'><slot/></button>
+  <button :class='[$style.btn, $style.animation,
+  {
+    [$style.red]: props.color === "red"
+  },
+  {
+    [$style.grey]: props.color === "grey"
+  },
+  {
+    [$style.orange]: props.color === "orange"
+  }]'>
+    <slot />
+  </button>
 </template>
 <style lang="scss" module>
 .btn {
- @include primeBtn($grey,15rem,5rem)
+  @include primeBtn(15rem, 5rem)
 }
-.grey{
+
+.grey {
   @include colorBtn($grey);
-  &:hover{
+
+  &:hover {
     @include colorBtnHover($grey)
   }
 }
-.orange{
-    @include colorBtn($orange);
-    width: 20rem;
-    &:hover{
-      @include colorBtnHover($orange)
-    }
+
+.orange {
+  @include colorBtn($orange);
+  width: 20rem;
+
+  &:hover {
+    @include colorBtnHover($orange)
+  }
 }
-.red{
-    @include colorBtn($red);
-    &:hover{
-      @include colorBtnHover($red)
-    }
+
+.red {
+  @include colorBtn($red);
+
+  &:hover {
+    @include colorBtnHover($red)
+  }
 }
-</style>
+
+@media(max-width:1380px) {
+  .btn {
+    @include primeBtn(15rem, 5rem);
+  }
+}
+
+@media(max-width:980px) {
+  .btn {
+    @include primeBtn(12rem, 4rem);
+    font-size: 1.4rem
+  }
+}
+
+@media(max-width:680px) {
+  .btn {
+    @include primeBtn(12rem, 4rem);
+    font-size: 1.2rem;
+  }
+
+  .orange {
+    width: 12rem;
+  }
+}</style>
--- client/src/components/organisms/Header.vue
+++ client/src/components/organisms/Header.vue
@@ -4,27 +4,92 @@ import Button from '../atoms/Button.vue';
 
 <template>
     <header :class="[$style.header, $style.container]">
-        <Button :class="[$style.btn,$style.loginBtn]" color="grey">Login</Button>
-        <Button :class="[$style.btn,$style.createBtn]" color="orange">Create account</Button>
-        <Button :class="[$style.btn,$style.playBtn]" color="red">Play</Button>
+        <div :class="$style.logo"></div>
+        <Button :class="[$style.btn, $style.loginBtn]" color="grey">Login</Button>
+        <Button :class="[$style.btn, $style.createBtn]" color="orange">Create account</Button>
+        <Button :class="[$style.btn, $style.playBtn]" color="red">Play</Button>
     </header>
 </template>
 
 <style lang="scss" module>
-    .header{
-        height: 18rem;
-        display: flex;
-        position: relative;
-        .btn{
+.header {
+    height: 18rem;
+    display: flex;
+    position: relative;
+
+    .logo {
+        background-image: url('../../assets/images/logo.png');
+        background-size: cover;
+        background-repeat: no-repeat;
+        width: 28rem;
+        height: 10rem;
+        position: absolute;
+        left: 38px;
+    }
+
+    .btn {
+        position: absolute;
+        bottom: 0rem;
+    }
+
+    .createBtn {
+        left: 15.5rem
+    }
+
+    .playBtn {
+        right: 0rem;
+        padding: 1.5rem 6rem;
+    }
+}
+
+@media(max-width:1380px) {
+    .header {
+        .logo {
+            left: 12px;
+        }
+    }
+}
+
+@media(max-width:980px) {
+    .header {
+        .createBtn {
+            left: 12.5rem
+        }
+
+        .logo {
+            left: 0px;
+        }
+    }
+}
+
+@media(max-width:680px) {
+    .header {
+        .btn {
             position: absolute;
             bottom: 0rem;
         }
-        .createBtn{
-            left: 15.5rem
+
+        .createBtn {
+            left: 0rem;
+            bottom: 4.5rem;
         }
-        .playBtn{
+
+        .playBtn {
             right: 0rem;
             padding: 1.5rem 6rem;
         }
+
+        .logo {
+            position: relative;
+            margin: 0 auto;
+            background-size: 100%;
+            width: 20rem;
+            height: 10rem;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            left: auto;
+        }
     }
+}
 </style>
